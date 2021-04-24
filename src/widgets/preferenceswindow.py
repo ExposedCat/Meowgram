c63f482f6bd1207d77566839d957e1be4eb69736
SeaDve <davecruz48@gmail.com>
2021-04-24 18:02:32 +0800

    ui: Add preferences window

---
+++ src/widgets/preferenceswindow.py
@@ -0,0 +1,8 @@
+from gi.repository import Handy, Gtk
+
+@Gtk.Template(resource_path="/com/github/ExposedCat/Meowgram/ui/preferenceswindow.ui")
+class MeowgramPreferencesWindow(Handy.PreferencesWindow):
+    __gtype_name__ = "MeowgramPreferencesWindow"
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
